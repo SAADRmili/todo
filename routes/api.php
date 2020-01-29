9beb9bf16cb244e40f2cca62516a54b7a58fe75d
SAADRMILI <saadrmili45@gmail.com>
2020-01-29 02:21:03 +0100

    Create task model

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+Route::resource('/tasks','TasksController',[
+    'except'=>['edit','show']
+]);
